2bd5270defa5a82d4136c13bb6552852bc6237a4
Eetu Tuomala <laardee@outlook.com>
2018-04-05 08:27:35 +0300

    add hosted zone to resources

--- src/handlers/record/index.js
+++ src/handlers/record/index.js
@@ -2,5 +2,11 @@
 
 module.exports.handler = (event, context, callback) => {
   console.log(JSON.stringify(event, null, 2));
-  return callback(null, 'ok');
+  let message = 'no change';
+  if (typeof event.Records !== 'undefined' && typeof event.Records[0] !== 'undefined') {
+    const ip = event.Records[0].dynamodb.NewImage.ip.S;
+    const record = event.Records[0].dynamodb.NewImage.record.S;
+    console.log(ip, record);
+  }
+  return callback(null, message);
 };
